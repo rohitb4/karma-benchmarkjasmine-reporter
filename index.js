08e0ad0f834fc4ec068b325e67fbdc69e5b177be
Rohit B <rohit.b@fusioncharts.com>
2014-02-27 16:59:32 +0530

    Added average

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var fs = require('fs');
 var colors = require('colors');
 var resultObj = {};
 var browsersList = {};
-var Table = require('cli-table');
+var Table = require('cli-table'),
+    browserCount = 0;
 
 
 var BenchmarkJasmineReporter = function(baseReporterDecorator, config, logger, helper, formatError) {
@@ -18,36 +19,54 @@ var BenchmarkJasmineReporter = function(baseReporterDecorator, config, logger, h
   browserListFile = outputFolder ? outputFolder + '/browser-list.json' : 'browser-list.json';
 
   baseReporterDecorator(this);
-
   
   this.onRunStart = function(browsers) {
     browsers.forEach(function(browser) {
       browsersList[browser.id] = browser;
+      browserCount++;
     });
   };
 
   this.onBrowserError = function(browser, error) {
-    console.log(error.red);
+    console.log(browser.name.cyan + ' ' +error.red);
   };
 
   this.onRunComplete = function() {
-    for (var suite in resultObj){
-      var suiteObj = resultObj[suite];
-      for (var spec in suiteObj) {
-        var specResult = suiteObj[spec];
-        var specResultMessage = '',
-            suiteHeader = suite + spec;
-
+    var avgTime,avgHz,
+        suite,
+        spec,
+        suiteObj,
+        specResult,
+        specResultMessage,
+        suiteHeader,
+        table,
+        browser,
+        browserSpecResult;
+    for (suite in resultObj){
+      suiteObj = resultObj[suite];
+      for (spec in suiteObj) {
+        specResult = suiteObj[spec];
+        specResultMessage = '';
+        suiteHeader = suite + spec;
+        avgTime = 0;
+        avgHz = 0;
         console.log(suiteHeader.cyan);
-        var table = new Table({
+        table = new Table({
           head:['Browser name', 'Time taken in sec', 'Operations/sec'],
           colWidths: [50,25,25]
         });
-        for(var browser in specResult) {
-          var browserSpecResult = specResult[browser];
+        for(browser in specResult) {
+          browserSpecResult = specResult[browser];
           table.push([browsersList[browser].name, browserSpecResult.time, browserSpecResult.hz]);
+          avgTime += browserSpecResult.time;
+          avgHz += browserSpecResult.hz;
         }
+        table.push(['Average Time', avgTime/browserCount, avgHz/browserCount]);
         console.log(table.toString());
+        specResult['average_time'] = {
+          time: avgTime/browserCount,
+          hz: avgHz/browserCount
+        };
       }
     }
     helper.mkdirIfNotExists(path.dirname(outputFile), function() {
