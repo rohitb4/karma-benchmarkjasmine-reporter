842ea76939818adf5908d29298cb3ef12c9064df
Rohit B <rohit.b@fusioncharts.com>
2014-03-14 12:15:09 +0530

    Support for colors in individual cells,fastest,slowest browser

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var colors = require('colors');
 var resultObj = {};
 var browsersList = {};
-var Table = require('cli-table'),
+var Table = require('./cli-table'),
     browserCount = 0;
 
 
@@ -22,7 +22,10 @@ var BenchmarkJasmineReporter = function(baseReporterDecorator, config, logger, h
   
   this.onRunStart = function(browsers) {
     browsers.forEach(function(browser) {
-      browsersList[browser.id] = browser;
+      browser.parsedName = browser.name.replace(/\s+/g, function() {
+        return '_bencho_';
+      });
+      browsersList[browser.parsedName] = browser;
       browserCount++;
     });
   };
@@ -41,7 +44,12 @@ var BenchmarkJasmineReporter = function(baseReporterDecorator, config, logger, h
         suiteHeader,
         table,
         browser,
-        browserSpecResult;
+        browserSpecResult,
+        fastestTime,
+        fastestIndex,
+        columns,
+        slowestTime,
+        slowestIndex;
     for (suite in resultObj){
       suiteObj = resultObj[suite];
       for (spec in suiteObj) {
@@ -50,16 +58,62 @@ var BenchmarkJasmineReporter = function(baseReporterDecorator, config, logger, h
         suiteHeader = suite + spec;
         avgTime = 0;
         avgHz = 0;
+        fastestTime = undefined;
+        slowestTime = undefined;
         console.log(suiteHeader.cyan);
         table = new Table({
           head:['Browser name', 'Time taken in sec', 'Operations/sec'],
           colWidths: [50,25,25]
         });
+        columns = {};
         for(browser in specResult) {
           browserSpecResult = specResult[browser];
-          table.push([browsersList[browser].name, browserSpecResult.time, browserSpecResult.hz]);
+          //table.push([browsersList[browser].name, {msg :browserSpecResult.time,style:['cyan']}, browserSpecResult.hz]);
+          columns[browser] = [browsersList[browser].name, browserSpecResult.time, browserSpecResult.hz];
           avgTime += browserSpecResult.time;
           avgHz += browserSpecResult.hz;
+          if (fastestTime !== undefined) {
+            if (fastestTime > browserSpecResult.time) {
+              fastestTime = browserSpecResult.time;
+              fastestIndex = browser;
+            }
+          }
+          else {
+            fastestTime = browserSpecResult.time;
+            fastestIndex = browser;
+          }
+          if (slowestTime !== undefined) {
+            if (slowestTime < browserSpecResult.time) {
+              slowestTime = browserSpecResult.time;
+              slowestIndex = browser;
+            }
+          }
+          else {
+            slowestTime = browserSpecResult.time;
+            slowestIndex = browser;
+          }
+
+        }
+        columns[fastestIndex][2] = {
+          msg: columns[fastestIndex][2],
+          style: ['green']
+        };
+        columns[fastestIndex][1] = {
+          msg: columns[fastestIndex][1],
+          style: ['green']
+        };
+        if (slowestIndex !== fastestIndex) {
+          columns[slowestIndex][2] = {
+            msg: columns[slowestIndex][2],
+            style: ['red']
+          };
+          columns[slowestIndex][1] = {
+            msg: columns[slowestIndex][1],
+            style: ['red']
+          };
+        }
+        for (browser in columns) {
+          table.push(columns[browser]);
         }
         table.push(['Average Time', avgTime/browserCount, avgHz/browserCount]);
         console.log(table.toString());
@@ -82,7 +136,7 @@ var BenchmarkJasmineReporter = function(baseReporterDecorator, config, logger, h
     if(!resultObj[result.suite][result.description]) {
       resultObj[result.suite][result.description] = {};
     }
-    resultObj[result.suite][result.description][browser.id] = {
+    resultObj[result.suite][result.description][browser.parsedName] = {
       success:result.success,
       time:result.time,
       hz:result.hz,
